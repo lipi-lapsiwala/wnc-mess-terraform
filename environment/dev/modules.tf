9edff1a464a03981ff55a6fd4cfe051240bce199
Lipi Lapsiwala <lipi.lapsiwala@gmail.com>
2025-04-09 06:18:20 +0000

    adding an app service terraform

--- environment/dev/modules.tf
+++ environment/dev/modules.tf
@@ -1,9 +1,11 @@
 # importing app service module
 module "wnc_mess_app_service" {
-  source                              = "mnt/d/Lipi/wnc-mess-terraform/modules/app-service"
+  source                              = "../../modules/app-service/"
+
   azure_app_service_plan_name         = var.azure_app_service_plan_name
   azure_location                      = var.azure_location
   azure_resource_group                = azurerm_resource_group.wnc_mess.name
-  azure_app_service_os_kind           = "Linux"
+  azure_app_service_os_kind           = "Windows"
   azure_app_service_reserved_instance = false
+  app_service_name = var.app_service_name
 }
--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -1,7 +1,12 @@
 variable "azure_location" {
-  default = "East US"
+  # One of the regions that typically have more availability for free tier
+  default     = "Central US"
 }
 
 variable "azure_app_service_plan_name" {
   default = "wnc-mess-app-service-plan"
+}
+
+variable "app_service_name" {
+  default = "wnc-mess-app-service"
 }
